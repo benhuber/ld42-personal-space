a4edb294912fa81f0da33e12e01d96e861f5ae7e
Amelia <amelia@mysteriousfox.com>
2018-08-11 17:07:02 +0200

    First Game and data manager script and prefabs

---
+++ Personal Space/Assets/Scripts/PersistentDataComponent.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentDataComponent : MonoBehaviour {
+
+	public enum EEndings
+	{
+		EEndings_StressOverload,
+		EEndings_AllQuestCompleted,
+		EEndings_BackHome,
+		EEndings_NumberOfEndings
+	}
+
+	private	bool[] endingsCompleted = new bool[(int)EEndings.EEndings_NumberOfEndings];
+	
+	void awake () {
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	void Start () {
+		
+	}
+	
+		void Update () {
+		
+	}
+
+	public bool IsEndingCompleted(EEndings ending)
+	{
+		return endingsCompleted[(int) ending];
+	}
+}
