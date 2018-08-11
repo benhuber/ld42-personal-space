6f719796e81331b9d42c8479be49dd7602f36283
radow <masteroftheriddles@googlemail.com>
2018-08-11 16:11:52 +0200

    player can trigger Dialog && Cleanup

--- Personal Space/Assets/Scripts/Talktome.cs
+++ Personal Space/Assets/Scripts/Talktome.cs
@@ -7,26 +7,43 @@ public class Talktome : MonoBehaviour {
     public Dialogbox.Dialogstate[] ds;
     public Dialogbox.Dialogtransition[] dt;
 
+    public bool instigatesDialog = true;
+    public bool resets = false;
+    public float resettime = 10f;
+    bool keypressed;
 
     public GameObject Dialog;
     public LayerMask Lplayer;
 
     bool inDialog = false;
-    public float talkradius = 2f;
+    public float talkradius = .6f;
 
     bool done = false;
+    float doneuntill;
+    
 
-	
+
+    private void Update()
+    {
+        keypressed = Input.GetKey(KeyCode.E);
+    }
+
     void FixedUpdate () {
+        if (Time.fixedTime > doneuntill) done = false;
         if (done) return;
+
         Collider2D player = Physics2D.OverlapCircle(transform.position, talkradius, Lplayer);
         if (player != null)
         {
+            if (!instigatesDialog && !keypressed) return;
             Dialog.SetActive(true);
             Dialogbox.Dialogsystem.StartDialog(ds, dt);
             done = true;
+            doneuntill = Time.fixedTime + resettime;
         }
 	}
+
+
 
     private void OnDrawGizmos()
     {
