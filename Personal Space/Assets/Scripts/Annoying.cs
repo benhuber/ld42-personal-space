042124725f410ee174f1dc9b6b9659a061016df8
radow <masteroftheriddles@googlemail.com>
2018-08-12 11:57:50 +0200

    new stressbar

--- Personal Space/Assets/Scripts/Annoying.cs
+++ Personal Space/Assets/Scripts/Annoying.cs
@@ -4,5 +4,16 @@ using UnityEngine;
 
 public class Annoying : MonoBehaviour {
 
+    public SpriteRenderer sr;
     public float value = 1f;
+    public bool setColorBasedOnValue;
+
+    private void FixedUpdate()
+    {
+        if (setColorBasedOnValue && sr != null)
+        {
+            Color c = Color.Lerp(Color.cyan, Color.red, value / 2);
+            sr.color = c;
+        }
+    }
 }
