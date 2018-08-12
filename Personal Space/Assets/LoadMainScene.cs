1fcdd90a40ff31f2788d0cea6fcc6c2abc8a75b6
Ben Huber <ben.huber@hemio.de>
2018-08-12 20:19:04 +0200

    load scene asynchronously

--- Personal Space/Assets/LoadMainScene.cs
+++ Personal Space/Assets/LoadMainScene.cs
@@ -4,7 +4,17 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LoadMainScene : MonoBehaviour {
+	AsyncOperation sceneLoader;
+
+	private void Update() {
+		if (sceneLoader == null) {
+			// needs to be here instead of Awake due to a known bug...
+			sceneLoader = SceneManager.LoadSceneAsync(1);
+			sceneLoader.allowSceneActivation = false;
+		}
+	}
+
 	public void BtnPress() {
-		SceneManager.LoadScene(1);
+		sceneLoader.allowSceneActivation = true;
 	}
 }
