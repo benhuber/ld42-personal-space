0dd756d9b961cbd400de273593655c013d97df17
radow <masteroftheriddles@googlemail.com>
2018-08-12 02:20:54 +0200

    basic Endscreens implemented, Safe Person (darya) implimented

---
+++ Personal Space/Assets/PLACEHOLDER_DATA.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLACEHOLDER_DATA : MonoBehaviour {
+
+    public enum Endings {Default, AllFriendsDone, Stay, Stress, Time}
+    public Endings ending;
+    public static PLACEHOLDER_DATA data;
+
+	// Use this for initialization
+	void Awake () {
+        data = this;
+	}
+}
