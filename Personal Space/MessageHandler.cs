8445822a7d3302c43390761844218acbf3ee7a81
radow <masteroftheriddles@googlemail.com>
2018-08-12 12:34:58 +0200

    added new Stressbar & messagehandler

---
+++ Personal Space/MessageHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MessageHandler : MonoBehaviour {
+
+    public RectTransform Rtext;
+    public TextMeshProUGUI text;
+
+    Vector3 originalPos;
+
+    bool coming;
+    bool waiting;
+    bool going;
+
+    public float Debug;
+
+	// Use this for initialization
+	void Start () {
+        originalPos = text.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug = Rtext.transform.position.x;
+
+        if (coming)
+        {
+            if(Rtext.transform.position.x >= 0f) { }
+
+        }
+		
+	}
+
+
+}
