ee42c285315cb0dff07e371159b452da34891b3c
radow <masteroftheriddles@googlemail.com>
2018-08-12 20:24:55 +0200

    cleanup stuff, setup for events

---
+++ Personal Space/Assets/Event.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Event : MonoBehaviour {
+
+    public GameObject icon;
+    public GameObject scene;
+
+    bool growing;
+    bool extended;
+    bool shrinking;
+
+    Vector3 origin;
+
+    public float speed = 1.5f;
+    public float amplitude = .6f;
+
+    public LayerMask player;
+    public float radius = 1f; 
+
+
+	void Start () {
+        origin = icon.transform.localScale;
+	}
+
+    private void Update()
+    {
+        if(1-Time.timeScale<.1) icon.transform.localScale = origin + new Vector3(amplitude*Mathf.Sin(Time.time*speed), amplitude * Mathf.Sin(Time.time * speed), 0);
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        
+        Collider2D c = Physics2D.OverlapCircle(transform.position, radius, player);
+        if (c!=null)
+        {
+            if (!extended) growing = true;
+        }
+	}
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
