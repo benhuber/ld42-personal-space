b925f045ec9378a2ef6fddaf3613aafaae16c6b8
radow <masteroftheriddles@googlemail.com>
2018-08-13 03:52:01 +0200

    archievements

--- Personal Space/Assets/Scripts/PLACEHOLDER_DATA.cs
+++ Personal Space/Assets/Scripts/PLACEHOLDER_DATA.cs
@@ -9,6 +9,15 @@ public class PLACEHOLDER_DATA : MonoBehaviour {
     public static PLACEHOLDER_DATA data;
     public int numberOfFriendsSpokenTo=0;
 
+    //archievements
+    public bool NightOwl = false;
+    public bool FreeLikeABird = false;
+    public bool ThereWillBeCake = false;
+    public bool Bookclub = false;
+    public bool Prosocial = false;
+    public bool FancyDancer = false;
+ 
+
 	// Use this for initialization
 	void Awake () {
         data = this;
