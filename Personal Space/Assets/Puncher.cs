1d8b00a618e02ce79692b3bc7b1e7fcfed12cf8d
Ben Huber <ben.huber@hemio.de>
2018-08-13 17:38:39 +0200

    punching dude

---
+++ Personal Space/Assets/Puncher.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puncher : MonoBehaviour {
+
+	GameObject punchingEvent;
+	CrowdMovement movement;
+	Rigidbody2D rb;
+	public Transform punchPosition;
+
+	private void Start() {
+		punchingEvent = transform.Find("Punch").gameObject;
+		punchingEvent.SetActive(false);
+		movement = GetComponent<CrowdMovement>();
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	float myTime = 0f;
+	private void FixedUpdate() {
+		myTime += Time.fixedDeltaTime;
+		if (myTime % 180 > 120) {
+			// punching
+			if ((punchPosition.position - transform.position).magnitude > 0.5f) {
+				movement.ForceWalkTo(punchPosition.position);
+			} else {
+				movement.enabled = false;
+				punchingEvent.SetActive(true);
+				rb.velocity = new Vector3();
+			}
+		} else {
+			// crowding
+			punchingEvent.SetActive(false);
+			movement.enabled = true;
+		}
+	}
+}
