5f937ea4e358ee2ec751ebf7e1ca6cc4926fff80
Amelia <amelia@mysteriousfox.com>
2018-08-13 15:27:20 +0200

    Addded ending scene. Updated data component

--- Personal Space/Assets/Scripts/PersistentDataComponent.cs
+++ Personal Space/Assets/Scripts/PersistentDataComponent.cs
@@ -7,13 +7,28 @@ public class PersistentDataComponent : MonoBehaviour {
 
 	public enum EEndings
 	{
-		EEndings_StressOverload,
-		EEndings_AllQuestCompleted,
-		EEndings_BackHome,
+		EEndings_Default,
+		EEndings_AllFriendsDone,
+		EEndings_Stay,
+		EEndings_Stress,
+		EEndings_Time,
 		EEndings_NumberOfEndings
 	}
 
+	public enum EAchievement
+	{
+		EAchievement_NightOwl,
+		EAchievement_FreeLikeABird,
+		EAchievement_ThereWillBeCake,
+		EAchievement_Bookclub,
+		EAchievement_Prosocial, // ?
+		EAchievement_FancyDancer, // ?
+		EAchievement_NumberOfAchievement
+	}
+
 	private	bool[] endingsCompleted = new bool[(int)EEndings.EEndings_NumberOfEndings];
+	private	bool[] achievementsCompleted = new bool[(int)EEndings.EEndings_NumberOfEndings];
+
 	private static bool created = false;
 	
 	void Start () {
@@ -37,5 +52,17 @@ public class PersistentDataComponent : MonoBehaviour {
 	{
 		Assert.IsTrue((int) ending < (int) EEndings.EEndings_NumberOfEndings);
 		return endingsCompleted[(int) ending];
+	}
+
+	public void CompleteAnAchievment(EAchievement achievement)
+	{
+		Assert.IsTrue((int) achievement < (int) EAchievement.EAchievement_NumberOfAchievement);
+		achievementsCompleted[(int) achievement] = true;
+	}
+
+	public bool IsAchievmentCompleted(EAchievement achievement)
+	{
+		Assert.IsTrue((int) achievement < (int) EAchievement.EAchievement_NumberOfAchievement);
+		return achievementsCompleted[(int) achievement];
 	}
 }
